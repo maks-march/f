f42e8779163cce54733b9068d55fef023e4fa1fa
maks-march <1maks_2055@mail.ru>
2022-02-07 22:11:48 +0500

    d

---
+++ script.js
@@ -0,0 +1,9 @@
+const text = document.getElementById('love');
+
+text.addEventListener('click',() => {
+    if (text.innerText !== 'Данис любит иру') {
+        text.innerText = 'Данис любит иру';
+    } else {
+    text.innerText = 'Данис ебет иру';
+    }
+});
