ac59519042d9646a5cc1c0aa8f078a32cded1d18
maks-march <98648053+maks-march@users.noreply.github.com>
2024-03-18 19:29:45 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const text = document.getElementById('love');
-
-text.addEventListener('click',() => {
-    if (text.innerText !== 'Данис любит иру') {
-        text.innerText = 'Данис любит иру';
-    } else {
-    text.innerText = 'Данис  ебет иру';
-    }
-});
